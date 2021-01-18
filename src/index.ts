39e9f58540fc75861a6f5516756aefd303b4e6e2
Santiago Bazerque <sbazerque@gmail.com>
2021-01-18 16:26:06 -0300

    Added a per-ChatRoom  confguration object, intended for standalone use (e.g. embedded in a website)

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 export { ChatRoom } from './model/ChatRoom';
 export { ChatMessage } from './model/ChatMessage';
+export { ChatRoomConfig } from './model/ChatRoomConfig';
---
+++ src/model/ChatRoomConfig.ts
@@ -0,0 +1,44 @@
+import { HashedLiteral, HashedObject, Identity, MutableReference } from '@hyper-hyper-space/core';
+
+class ChatRoomConfig extends HashedObject {
+
+    static className = 'p2p-chat/v0/model/ChatRoomConfig';
+
+    chatRoomWordCode?: string[];
+    chatRoomWordCodeLang?: string; 
+    authorIdentity?: MutableReference<Identity>;
+    archiveLocally?: MutableReference<HashedLiteral>;
+
+    constructor(chatRoomWordCode?: string[], chatRoomWordCodeLang='en') {
+        super();
+
+        if (chatRoomWordCode !== undefined) {
+            this.chatRoomWordCode = chatRoomWordCode;
+            this.chatRoomWordCodeLang = chatRoomWordCodeLang;
+            this.addDerivedField('authorIdentity', new MutableReference());
+            this.addDerivedField('archiveLocally', new MutableReference());
+        }
+    }
+    
+    getClassName(): string {
+        return ChatRoomConfig.className;
+    }
+
+    init(): void {
+        
+    }
+
+    validate(_references: Map<string, HashedObject>): boolean {
+        return this.chatRoomWordCode !== undefined &&
+               this.chatRoomWordCodeLang !== undefined &&
+               this.checkDerivedField('authorIdentity') &&
+               this.authorIdentity instanceof MutableReference &&
+               this.checkDerivedField('archiveLocally') &&
+               this.archiveLocally instanceof MutableReference;
+    }
+
+}
+
+HashedObject.registerClass(ChatRoomConfig.className, ChatRoomConfig);
+
+export { ChatRoomConfig };
