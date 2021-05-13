5003394eaeb242cc7a0e9995c624d352b9691a50
Santiago Bazerque <sbazerque@gmail.com>
2021-05-13 11:13:09 -0300

    added missing id to chat config object

--- src/model/ChatRoomConfig.ts
+++ src/model/ChatRoomConfig.ts
@@ -1,4 +1,4 @@
-import { HashedLiteral, HashedObject, Identity, MutableReference } from '@hyper-hyper-space/core';
+import { HashedLiteral, HashedObject, Hashing, Identity, MutableReference } from '@hyper-hyper-space/core';
 
 class ChatRoomConfig extends HashedObject {
 
@@ -15,6 +15,7 @@ class ChatRoomConfig extends HashedObject {
         if (chatRoomWordCode !== undefined) {
             this.chatRoomWordCode = chatRoomWordCode;
             this.chatRoomWordCodeLang = chatRoomWordCodeLang;
+            this.setId(this.computeId());
             this.addDerivedField('authorIdentity', new MutableReference());
             this.addDerivedField('archiveLocally', new MutableReference());
         }
@@ -31,12 +32,16 @@ class ChatRoomConfig extends HashedObject {
     async validate(_references: Map<string, HashedObject>): Promise<boolean> {
         return this.chatRoomWordCode !== undefined &&
                this.chatRoomWordCodeLang !== undefined &&
+               this.getId() === this.computeId() &&
                this.checkDerivedField('authorIdentity') &&
                this.authorIdentity instanceof MutableReference &&
                this.checkDerivedField('archiveLocally') &&
                this.archiveLocally instanceof MutableReference;
     }
 
+    private computeId() {
+        return Hashing.forString(this.chatRoomWordCodeLang + '/' + (this.chatRoomWordCode as string[]).join('-'));
+    }
 }
 
 HashedObject.registerClass(ChatRoomConfig.className, ChatRoomConfig);
